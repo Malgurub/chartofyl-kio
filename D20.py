0cd1325081abb5f039757000a4cbf5b78572d80f
Samuel Alvaro <127910900+Malgurub@users.noreply.github.com>
2023-05-24 19:39:59 -0300

    D20.py
    Optimization

--- D20.py
+++ D20.py
@@ -1,12 +1,4 @@
-import random
-
-d4 = random.randint(1, 4)
-d6 = random.randint(1, 6)
-d8 = random.randint(1, 8)
-d10 = random.randint(1, 10)
-d12 = random.randint(1, 12)
-d20 = random.randint(1, 20)
-d100 = random.randint(1, 100)
+from random import randint
 
 continuar = 'sim'
 
@@ -14,18 +6,25 @@ while continuar == 'sim':
     rolagem = input('Qual dado quer rolar? (d4, d6, d8, d10, d12, d20, d100) ')
 
     if rolagem == 'd4':
+        d4 = randint(1, 4)
         print(f'Você tirou {d4}')
     elif rolagem == 'd6':
+        d6 = randint(1, 6)
         print(f'Você tirou {d6}')
     elif rolagem == 'd8':
+        d8 = randint(1, 8)
         print(f'Você tirou {d8}')
     elif rolagem == 'd10':
+        d10 = randint(1, 10)
         print(f'Você tirou {d10}')
     elif rolagem == 'd12':
+        d12 = randint(1, 12)
         print(f'Você tirou {d12}')
     elif rolagem == 'd20':
+        d20 = randint(1, 20)
         print(f'Você tirou {d20}')
     elif rolagem == 'd100':
+        d100 = randint(1, 100)
         print(f'Você tirou {d100}')
     else:
         print('Opção inválida! Por favor, escolha um dado válido.')
